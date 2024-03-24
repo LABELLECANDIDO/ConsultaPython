e8db62ddfd8a39f4915d6dbbc080aa3638a2c71e
Labelle Candido <labellecandido@icloud.com>
2024-03-24 19:40:54 -0300

    finished

--- webscraping.py
+++ webscraping.py
@@ -1,26 +1,32 @@
 import requests
 from bs4 import BeautifulSoup #extração de dados
+import pandas as pd
 
 url = "https://www.mercadolivre.com.br/"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
 }#ajuda na compatibilidade
 
 requisicao = requests.get(url, headers=headers)
-print(requisicao)
-
+#print(requisicao)
 site = BeautifulSoup(requisicao.text, "html.parser")
 
-valores = site.find_all("span", class_="andes-money-amount--cents-superscript")
+valores = site.find_all("span", class_="andes-money-amount__fraction")
 titulo = site.find_all("a", class_="poly-component__title")
 
+links = ['https://www.mercadolivre.com.br/' + titulo['href'] for titulo in titulo]
 
-for preco in valores: #armazena cada item da lista valores na variavel preco
-    valor_extraido = preco.get_text()#extrair texto
-    print(valor_extraido)
+#listas separadas
+produto, valor, link = [], [], []
+for titulo, preco in zip(titulo, valores):
+    produto.append(titulo.get_text().strip())
+    valor.append(preco.get_text().strip())  
+    link.append('https://www.mercadolivre.com.br/' + titulo['href']) 
 
-    #for tituloPreco in titulo:
-       # titulo_extraido = tituloPreco.get_text()
-   #  print(titulo_extraido)
+
+# Criar o DataFrame fora do loop
+df = pd.DataFrame({'Produto': produto, 'Preço': valor, 'Link': link})
+
+df.head(10)
 
 
 
